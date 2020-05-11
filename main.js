20b2299b248ba44c2720ee63794ebf0fd5ca3c9e
tinge666 <997494540@qq.com>
2020-05-11 12:26:16 +0800

    提交修改

--- main.js
+++ main.js
@@ -68,6 +68,25 @@ window.boot = function () {
     var onStart = function () {
         cc.loader.downloader._subpackages = settings.subpackages;
 
+        cc.ContainerStrategy.prototype._setupContainer = function (view, w, h) {
+            var locCanvas = cc.game.canvas, locContainer = cc.game.container;
+            if (!CC_WECHATGAME && cc.sys.os === cc.sys.OS_ANDROID) {
+                document.body.style.width = (view._isRotated ? h : w) + 'px';
+                document.body.style.height = (view._isRotated ? w : h) + 'px';
+            }
+            // Setup style
+            locContainer.style.width = locCanvas.style.width = w + 'px';
+            locContainer.style.height = locCanvas.style.height = h + 'px';
+            // Setup pixel ratio for retina display
+            var devicePixelRatio = view._devicePixelRatio = 1;
+            if (view.isRetinaEnabled()) {
+                devicePixelRatio = view._devicePixelRatio = window.devicePixelRatio || 1;
+            }
+            // Setup canvas
+            locCanvas.width = w * devicePixelRatio;
+            locCanvas.height = h * devicePixelRatio;
+        };
+
         cc.view.enableRetina(true);
         cc.view.resizeWithBrowserSize(true);
 
